8064efcd8b750cf136bd6ae7dd68c906912ba92e
stillinbeta77 <info@savageonlinemarketing.com>
2015-02-17 15:50:42 -0700

    trying to get the list functionality to work

---
+++ js/app.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	$(".add-btn").click(function (e) {
+	    e.preventDefault()
+	    var add = $('#listInput').val();
+        $("ol.list-items").append('<li class="on">' + add + '</li>');
+
+    });
+
+
+	$(document).on('click', '.on', function () {
+	$(".on").wrap("<strike>");
+	$(".on").fadeOut("slow");
+
+	});
+
+});
